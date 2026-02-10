c1505185afb33048075f6f802f7862ad387dec4c
Gabriel Galani <gabrielgalaniipt@gmail.com>
2026-02-09 21:28:16 -0300

    feat(setup) Clear volumes not used

--- src/setup/03_volumes.sql
+++ src/setup/03_volumes.sql
@@ -1,8 +1,5 @@
 USE CATALOG olist_lakehouse;
 
-CREATE VOLUME IF NOT EXISTS bronze.raw_volume
-COMMENT 'Raw data landing zone for Auto Loader';
-
 CREATE VOLUME IF NOT EXISTS cdc.cdc_volume
 COMMENT 'CDC incoming data';
 
